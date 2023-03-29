6dfe0dd6c0e36cdf33121a7e39e990d612a0a81b
gilmariano <marianog88@gmail.com>
2023-03-29 03:02:27 -0300

    Agregado de autor en detalles y estetica a pesca_formr

--- AppPesca/views.py
+++ AppPesca/views.py
@@ -28,6 +28,11 @@ class PescaDetail(DetailView):
     model = Pesca
     context_object_name = "pescado"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['autor'] = self.object.propietario.username
+        return context
+
 
 class PescaUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Pesca
@@ -53,8 +58,13 @@ class PescaDelete(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
 class PescaCreate(LoginRequiredMixin, CreateView):
     model = Pesca
-    success_url = reverse_lazy("pescado-list")
-    fields = '__all__'
+    success_url = reverse_lazy("pescado-mine")
+    fields = ['nombre', 'detalle', 'peso', 'imagen']
+
+    def form_valid(self, form):
+        form.instance.propietario = self.request.user
+        form.instance.autor = self.request.user.username
+        return super().form_valid(form)
 
 
 class PescaSearch(ListView):
